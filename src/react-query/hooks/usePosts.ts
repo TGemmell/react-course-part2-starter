d214d55c92f5b129ab953e33339439d66bcd7837
Tom <Gemmell94@hotmail.co.uk>
2023-08-29 20:53:54 +0100

    added paging

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -8,18 +8,25 @@ export interface Post {
     userId: number;
   }
 
-const usePosts = (userId: number | undefined) => {
+export interface PostQuery {
+    page: number;
+    pageSize: number;
+}
+
+const usePosts = (query: PostQuery) => {
     const fetchPosts = axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
         params: {
-            userId
+            _start: (query.page - 1) * query.pageSize,
+            _limit: query.pageSize
         }
     })
     .then(res => res.data);
 
     return useQuery<Post[], Error>({
-        queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+        queryKey: ["posts", query],
         queryFn: () => fetchPosts,
-        staleTime: 1 * 60 * 1000 // 1 minute
+        staleTime: 1 * 60 * 1000, // 1m
+        keepPreviousData: true
     });
 }
 
