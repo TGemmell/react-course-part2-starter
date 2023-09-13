6591811abecae731acd313020da9f3256b9ec51a
Tom <Gemmell94@hotmail.co.uk>
2023-09-13 11:25:39 +0100

    Added the tasks context
    Allows us to pass state between files without drilling into the parents

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useReducer } from "react";
 import "./App.css";
-import LoginStatus from "./state-management/LoginStatus";
-import TaskList from "./state-management/TaskList";
+import TasksContext from "./state-management/contexts/tasksContext";
+import tasksReducer from "./state-management/reducers/tasksReducer";
+import NavBar from "./state-management/NavBar";
+import HomePage from "./state-management/HomePage";
 
 function App() {
-  return <LoginStatus />;
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+
+  return (
+    <TasksContext.Provider value={{ tasks, dispatch }}>
+      <NavBar />
+      <HomePage />
+    </TasksContext.Provider>
+  );
 }
 
 export default App;
